aa8ec78e925060449fcb29a02045b295c32b52df
Santiago Ramírez <35697573+santrami@users.noreply.github.com>
2022-06-18 20:56:38 -0500

    responsive4

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "78410b50d4af316d"; }
+/******/ 		__webpack_require__.h = function() { return "e423bc86683e5d07"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
